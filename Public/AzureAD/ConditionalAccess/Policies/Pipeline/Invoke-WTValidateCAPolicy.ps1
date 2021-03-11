4aad66defb03c23725e3c364b238ed65e1af8a10
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-03-11 20:53:38 +0000

    Add validation to import function

--- Public/AzureAD/ConditionalAccess/Policies/Pipeline/Invoke-WTValidateCAPolicy.ps1
+++ Public/AzureAD/ConditionalAccess/Policies/Pipeline/Invoke-WTValidateCAPolicy.ps1
@@ -1,8 +1,8 @@
 <#
 .Synopsis
-    Import all Conditional Access policies from JSON definition
+    Import all Conditional Access policies from JSON definition and validate
 .Description
-    This function imports the Conditional Access policies from JSON using the Microsoft Graph API.
+    This function imports the Conditional Access policies from JSON using the Microsoft Graph API and runs validation checks.
     The following Microsoft Graph API permissions are required for the service principal used for authentication:
         Policy.ReadWrite.ConditionalAccess
         Policy.Read.All
@@ -56,11 +56,18 @@ function Invoke-WTValidateCAPolicy {
             ValueFromPipeLineByPropertyName = $true,
             HelpMessage = "The directory path(s) of which all JSON file(s) will be imported"
         )]
-        [string]$Path
+        [string]$Path,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Specify whether files should be imported only, and not validated"
+        )]
+        [switch]$ImportOnly
     )
     Begin {
         try {
-
+            # Variables
+            $RequiredProperties = @("displayName", "grantControls", "conditions", "state", "sessionControls")
         }
         catch {
             Write-Error -Message $_.Exception
@@ -89,23 +96,120 @@ function Invoke-WTValidateCAPolicy {
 
             # If a file has been imported, convert from JSON to an object for deployment
             if ($ConditionalAccessPolicies) {
+
                 $ConditionalAccessPolicies = $ConditionalAccessPolicies | ConvertFrom-Json
                 
                 # Output current action
                 Write-Host "Importing Conditional Access Policies"
                 Write-Host "Policies: $($ConditionalAccessPolicies.count)"
                 
-                foreach ($Policy in $ConditionalAccessPolicies){
+                foreach ($Policy in $ConditionalAccessPolicies) {
                     Write-Host "Import: Policy Name: $($Policy.displayName)"
                 }
-                
-                # TODO: Validate import contains mandatory fields
+
+                # If there are policies imported, run validation checks
                 if ($ConditionalAccessPolicies) {
 
-                    $ValidateCAPolicies = $ConditionalAccessPolicies
-                    
-                    # Return policies
-                    $ValidateCAPolicies
+                    # If import only is set, return policies without validating
+                    if ($ImportOnly) {
+                        $ConditionalAccessPolicies
+                    }
+                    else {
+                        
+                        # Output current action
+                        Write-Host "Validating Conditional Access Policies"
+    
+                        # For each policy, run validation checks
+                        $InvalidPolicies = foreach ($Policy in $ConditionalAccessPolicies) {
+                            $PolicyValidate = $null
+    
+                            # Check for missing properties
+                            $PolicyProperties = $null
+                            $PolicyProperties = ($Policy | Get-Member -MemberType NoteProperty).name
+                            $PropertyCheck = $null
+
+                            # Check whether each required property, exists in the list of properties for the object
+                            $PropertyCheck = foreach ($Property in $RequiredProperties) {
+                                if ($Property -notin $PolicyProperties) {
+                                    $Property
+                                }
+                            }
+
+                            # Check for missing grant or session controls
+                            $ControlsCheck = $null
+                            $ControlsCheck = if (!$Policy.GrantControls) {
+                                if (!$Policy.sessioncontrols) {
+                                    Write-Output "No grant or session controls specified, at least one must be specified"
+                                }
+                            }
+
+                            # Check for missing conditions (under applications)
+                            $ApplicationsProperties = $null
+                            $ApplicationsProperties = ($Policy.conditions.applications | Get-Member -MemberType NoteProperty).name
+                            $ConditionsCheck = $null
+
+                            # For each condition, return true if a value exists for each condition checked
+                            $ConditionsCheck = foreach ($Condition in $ApplicationsProperties) {
+                                if ($Policy.conditions.applications.$Condition) {
+                                    $true
+                                }
+                            }
+    
+                            # If true is not in the condition check variable, it means there were no conditions that had a value
+                            if ($true -notin $ConditionsCheck) {
+                                $ConditionsCheck = Write-Output "No application conditions specified, at least one must be specified"
+                            }
+                            else {
+                                $ConditionsCheck = $null
+                            }
+    
+                            # Build and return object
+                            if ($PropertyCheck -or $ControlsCheck -or $ConditionsCheck) {
+                                $PolicyValidate = [ordered]@{}
+                                $PolicyValidate.Add("DisplayName", $Policy.displayName)
+                            }
+                            if ($PropertyCheck) {
+                                $PolicyValidate.Add("MissingProperties", $PropertyCheck)
+                            }
+                            if ($ControlsCheck) {
+                                $PolicyValidate.Add("MissingControls", $ControlsCheck)
+                            }
+                            if ($ConditionsCheck) {
+                                $PolicyValidate.Add("MissingConditions", $ConditionsCheck)
+                            }
+                            if ($PolicyValidate) {
+                                [pscustomobject]$PolicyValidate
+                            }
+                        }
+
+                        # Return validation result for each policy
+                        if ($InvalidPolicies) {
+                            foreach ($Policy in $InvalidPolicies) {
+                                Write-Warning "Validation failed for $($Policy.displayName)"
+                                if ($Policy.MissingProperties) {
+                                    Write-Warning "Required properties not present: $($Policy.MissingProperties)"
+                                }
+                                if ($Policy.MissingControls) {
+                                    Write-Warning "$($Policy.MissingControls)"
+                                }
+                                if ($Policy.MissingConditions) {
+                                    Write-Warning "$($Policy.MissingConditions)"
+                                }
+                            }
+    
+                            # Abort import
+                            $ErrorMessage = "Validation of policies was not successful, review configuration files and any warnings generated"
+                            Write-Error $ErrorMessage
+                            throw $ErrorMessage
+                        }
+                        else {
+
+                            # Return validated policies
+                            Write-Host "All policies have passed validation for required properties, controls and conditions"
+                            $ValidPolicies = $ConditionalAccessPolicies
+                            $ValidPolicies
+                        }
+                    }
                 }
                 else {
                     $WarningMessage = "No Conditional Access policies to be imported, import may have failed or none may exist"

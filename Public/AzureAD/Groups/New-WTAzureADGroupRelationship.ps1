dd089e572718eedca3290f465f09b1b3f43a4994
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-03-17 16:20:39 +0000

    Function to add group relationship

---
+++ Public/AzureAD/Groups/New-WTAzureADGroupRelationship.ps1
@@ -0,0 +1,140 @@
+function New-WTAzureADGroupRelationship {
+    [cmdletbinding()]
+    param (
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Client ID for the Azure AD service principal with Azure AD group Graph permissions"
+        )]
+        [string]$ClientID,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Client secret for the Azure AD service principal with Azure AD group Graph permissions"
+        )]
+        [string]$ClientSecret,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The initial domain (onmicrosoft.com) of the tenant"
+        )]
+        [string]$TenantDomain,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The access token, obtained from executing Get-WTGraphAccessToken"
+        )]
+        [string]$AccessToken,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Specify whether to exclude features in preview, a production API version will then be used instead"
+        )]
+        [switch]$ExcludePreviewFeatures,
+        [parameter(
+            Mandatory = $true,
+            ValueFromPipeLineByPropertyName = $true,
+            ValueFromPipeLine = $true,
+            HelpMessage = "The Azure AD group to add the members or owners to, this must contain valid id(s)"
+        )]
+        [Alias("GroupID")]
+        [string]$ID,
+        [parameter(
+            Mandatory = $true,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The group relationship to add, such as group members or owners"
+        )]
+        [ValidateSet("members", "owners")]
+        [string]$Relationship,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The relationship ids of the objects to add to the group"
+        )]
+        [Alias('RelationshipID', 'GroupRelationshipID', 'GroupRelationshipIDs')]
+        [string[]]$RelationshipIDs
+    )
+    Begin {
+        try {
+            # Function definitions
+            $Functions = @(
+                "GraphAPI\Public\Authentication\Get-WTGraphAccessToken.ps1",
+                "GraphAPI\Private\Invoke-WTGraphPost.ps1"
+            )
+
+            # Function dot source
+            foreach ($Function in $Functions) {
+                . $Function
+            }
+
+            # Variables
+            $Activity = "Adding Azure AD group $Relationship"
+            $Uri = "groups"
+            $CleanUpProperties = (
+                "id",
+                "createdDateTime",
+                "modifiedDateTime"
+            )
+
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+    Process {
+        try {
+
+            # If there is no access token, obtain one
+            if (!$AccessToken) {
+                $AccessToken = Get-WTGraphAccessToken `
+                    -ClientID $ClientID `
+                    -ClientSecret $ClientSecret `
+                    -TenantDomain $TenantDomain
+            }
+            if ($AccessToken) {
+
+                # Build Parameters
+                $Parameters = @{
+                    AccessToken       = $AccessToken
+                    Uri               = "$Uri/$Id/$Relationship/`$ref"
+                    CleanUpProperties = $CleanUpProperties
+                    Activity          = $Activity
+                }
+                if ($ExcludePreviewFeatures) {
+                    $Parameters.Add("ExcludePreviewFeatures", $true)
+                }
+
+                # If there are IDs, for each, create an object with the ID
+                if ($RelationshipIDs) {
+                    $RelationshipObject = foreach ($RelationshipId in $RelationshipIDs) {
+                        [pscustomobject]@{
+                            "@odata.id" = "https://graph.microsoft.com/v1.0/directoryObjects/$RelationshipId"
+                        }
+                    }
+
+                    # Add group relationship
+                    Invoke-WTGraphPost `
+                        @Parameters `
+                        -InputObject $RelationshipObject
+                }
+                else {
+                    $ErrorMessage = "There are no group $Relationship to be added"
+                    Write-Error $ErrorMessage
+                }
+            }
+            else {
+                $ErrorMessage = "No access token specified, obtain an access token object from Get-WTGraphAccessToken"
+                Write-Error $ErrorMessage
+                throw $ErrorMessage
+            }
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+    End {
+        
+    }
+}

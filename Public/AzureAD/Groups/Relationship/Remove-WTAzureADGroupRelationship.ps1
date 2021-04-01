ccfd328b9cbc7b5e29390cc53e4a2c06c5a19feb
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-04-01 17:34:54 +0100

    New remove function

---
+++ Public/AzureAD/Groups/Relationship/Remove-WTAzureADGroupRelationship.ps1
@@ -0,0 +1,135 @@
+function Remove-WTAzureADGroupRelationship {
+    [cmdletbinding()]
+    param (
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Client ID for the Azure AD service principal with Azure AD group Graph permissions"
+        )]
+        [string]$ClientID,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Client secret for the Azure AD service principal with Azure AD group Graph permissions"
+        )]
+        [string]$ClientSecret,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The initial domain (onmicrosoft.com) of the tenant"
+        )]
+        [string]$TenantDomain,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The access token, obtained from executing Get-WTGraphAccessToken"
+        )]
+        [string]$AccessToken,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Specify whether to exclude features in preview, a production API version will be used instead"
+        )]
+        [switch]$ExcludePreviewFeatures,
+        [parameter(
+            Mandatory = $true,
+            ValueFromPipeLineByPropertyName = $true,
+            ValueFromPipeLine = $true,
+            HelpMessage = "The Azure AD group to remove the members or owners from, this must contain valid id(s)"
+        )]
+        [Alias("GroupID")]
+        [string]$ID,
+        [parameter(
+            Mandatory = $true,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The group relationship to add, such as group members or owners"
+        )]
+        [ValidateSet("members", "owners")]
+        [string]$Relationship,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The relationship ids of the objects to add to the group"
+        )]
+        [Alias('RelationshipID', 'GroupRelationshipID', 'GroupRelationshipIDs')]
+        [string[]]$RelationshipIDs
+    )
+    Begin {
+        try {
+            # Function definitions
+            $Functions = @(
+                "GraphAPI\Public\Authentication\Get-WTGraphAccessToken.ps1",
+                "GraphAPI\Private\Invoke-WTGraphDelete.ps1"
+            )
+
+            # Function dot source
+            foreach ($Function in $Functions) {
+                . $Function
+            }
+
+            # Variables
+            $Activity = "Removing Azure AD group $Relationship"
+            $Uri = "groups"
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+    Process {
+        try {
+
+            # If there is no access token, obtain one
+            if (!$AccessToken) {
+                $AccessToken = Get-WTGraphAccessToken `
+                    -ClientID $ClientID `
+                    -ClientSecret $ClientSecret `
+                    -TenantDomain $TenantDomain
+            }
+            if ($AccessToken) {
+
+                # Build Parameters
+                $Parameters = @{
+                    AccessToken       = $AccessToken
+                    Activity          = $Activity
+                }
+                if ($ExcludePreviewFeatures) {
+                    $Parameters.Add("ExcludePreviewFeatures", $true)
+                }
+
+                # If there are IDs, for each, remove the group relationship
+                if ($RelationshipIDs) {
+                    foreach ($RelationshipId in $RelationshipIDs) {
+                        
+                        # Remove group relationship
+                        Invoke-WTGraphDelete `
+                            @Parameters `
+                            -Uri "$Uri/$Id/$Relationship/$RelationshipId/`$ref"
+                    }
+                }
+                else {
+                    $ErrorMessage = "There are no group $Relationship to be removed"
+                    Write-Error $ErrorMessage
+                }
+            }
+            else {
+                $ErrorMessage = "No access token specified, obtain an access token object from Get-WTGraphAccessToken"
+                Write-Error $ErrorMessage
+                throw $ErrorMessage
+            }
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+    End {
+        try {
+            
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+}

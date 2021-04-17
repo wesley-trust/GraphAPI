ed2d3e87340e5b37faad43b90cd9979ce007f1e8
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-04-17 17:21:16 +0100

    Add role to directory object

---
+++ Public/AzureAD/Roles/New-WTAzureADRoleMember.ps1
@@ -0,0 +1,133 @@
+function New-WTAzureADRoleMember {
+    [CmdletBinding()]
+    param (
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Client ID for the Azure AD service principal with Azure AD group Graph permissions"
+        )]
+        [string]$ClientID,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Client secret for the Azure AD service principal with Azure AD group Graph permissions"
+        )]
+        [string]$ClientSecret,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The initial domain (onmicrosoft.com) of the tenant"
+        )]
+        [string]$TenantDomain,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The access token, obtained from executing Get-WTGraphAccessToken"
+        )]
+        [string]$AccessToken,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "Specify whether to exclude features in preview, a production API version will be used instead"
+        )]
+        [switch]$ExcludePreviewFeatures,
+        [parameter(
+            Mandatory = $true,
+            ValueFromPipeLineByPropertyName = $true,
+            ValueFromPipeLine = $true,
+            HelpMessage = "The Azure AD role to add to the directory object, this must contain valid id(s)"
+        )]
+        [Alias("RoleID")]
+        [string]$ID,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipeLineByPropertyName = $true,
+            HelpMessage = "The directory object ids to add to the Azure AD role"
+        )]
+        [Alias('DirectoryObjectID', "MemberIDs", "MemberID")]
+        [string[]]$DirectoryObjectIDs
+    )
+    Begin {
+        try {
+            # Function definitions
+            $Functions = @(
+                "GraphAPI\Public\Authentication\Get-WTGraphAccessToken.ps1",
+                "GraphAPI\Private\Invoke-WTGraphPost.ps1"
+            )
+
+            # Function dot source
+            foreach ($Function in $Functions) {
+                . $Function
+            }
+
+            # Variables
+            $Activity = "Adding Azure AD role to directory object"
+            $Uri = "directoryRoles"
+            $Relationship = "members"
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+    Process {
+        try {
+
+            # If there is no access token, obtain one
+            if (!$AccessToken) {
+                $AccessToken = Get-WTGraphAccessToken `
+                    -ClientID $ClientID `
+                    -ClientSecret $ClientSecret `
+                    -TenantDomain $TenantDomain
+            }
+            if ($AccessToken) {
+
+                # Build Parameters
+                $Parameters = @{
+                    AccessToken = $AccessToken
+                    Uri         = "$Uri/$Id/$Relationship/`$ref"
+                    Activity    = $Activity
+                }
+                if ($ExcludePreviewFeatures) {
+                    $Parameters.Add("ExcludePreviewFeatures", $true)
+                }
+
+                # If there are IDs, for each, create an appropriate object with the IDs
+                if ($DirectoryObjectIDs) {
+                    $DirectoryMemberObject = foreach ($DirectoryObjectId in $DirectoryObjectIDs) {
+                        [PSCustomObject]@{
+                            "@odata.id" = "https://graph.microsoft.com/v1.0/directoryObjects/$DirectoryObjectId"
+                        }
+                    }
+                    
+                    # Add role to directory object
+                    Invoke-WTGraphPost `
+                        @Parameters `
+                        -InputObject $DirectoryMemberObject
+                }
+                else {
+                    $ErrorMessage = "There are no directory objects to add the roles"
+                    Write-Error $ErrorMessage
+                }
+            }
+            else {
+                $ErrorMessage = "No access token specified, obtain an access token object from Get-WTGraphAccessToken"
+                Write-Error $ErrorMessage
+                throw $ErrorMessage
+            }
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+    End {
+        try {
+            
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.exception
+        }
+    }
+}
